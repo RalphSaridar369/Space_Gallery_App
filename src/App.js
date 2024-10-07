6b82399d2b45f2cb8c095d2d74366ec9a0d7c63d
RalphSaridar369 <ralphsaridar@hotmail.com>
2024-10-07 19:15:38 +0300

    Enhancing App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ItemDetailed from "./ItemDetailed";
+import Header from "./components/header/Header";
+import Home from "./Home";
+import About from "./About";
+import Apod from "./Apod";
+
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about/" element={<About />} />
+          <Route path="/apod/" element={<Apod />} />
+          <Route path="/:id" element={<ItemDetailed />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/header/Static.ts
@@ -0,0 +1,14 @@
+export const routes = [
+  {
+    link: "/",
+    text: "Home",
+  },
+  {
+    link: "/apod/",
+    text: "Apod",
+  },
+  {
+    link: "/about/",
+    text: "About",
+  },
+];
